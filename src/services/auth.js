cf1564fb0f89c5fd04d4c904c247b9d61e86a747
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2022-06-01 20:10:35 -0700

    still wrestling cors

--- src/services/auth.js
+++ src/services/auth.js
@@ -3,16 +3,15 @@ import request from 'superagent'
 // Sign Up
 export async function signUp(username, password) {
   const response = await request
-    .post('http://localhost:7890/api/v1/users/register')
+    .post('https://polar-reaches-12563.herokuapp.com/api/v1/users/register')
     .send({ username, password })
   console.log(response.body)
   return response.body
 }
-
 // Log In
 export async function logIn(username, password) {
   const response = await request
-    .post('http://localhost:7890/api/v1/users/login')
+    .post('https://polar-reaches-12563.herokuapp.com/api/v1/users/login')
     .send({ username, password })
     .withCredentials()
   if ((response.body = '401')) console.log(response.body)
@@ -22,7 +21,7 @@ export async function logIn(username, password) {
 // Log out
 export async function logOut() {
   const response = await request
-    .delete('http://localhost:7890/api/v1/users/login')
+    .delete('https://polar-reaches-12563.herokuapp.com/api/v1/users/login')
     .withCredentials()
   console.log(response.body)
   return response.body
