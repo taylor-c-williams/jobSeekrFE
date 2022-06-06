6efc747f181c95f9ee8acd5d0156e3e719cbd587
Taylor Callan Williams <taylor.callan.williams@gmail.com>
2022-06-06 14:42:28 -0700

    job sorting & mapped column rendering

--- src/components/kanban/Column.jsx
+++ src/components/kanban/Column.jsx
@@ -1,21 +1,10 @@
 import KanbanItem from './KanbanItem'
 import styles from './kanban.module.css'
 
-export default function Column({ jobs, columnName }) {
-  let columnJobs = []
-  const columnType = `job.${columnName}`
-  console.log(columnType)
-
-  for (let i = 0; i < jobs.length; i++) {
-    const job = jobs[i]
-    if (`${columnType}` === true) {
-      columnJobs.push(job)
-    }
-  }
-  console.log(columnJobs)
+export default function Column({ jobs }) {
   return (
     <div className={styles.column}>
-      {columnJobs.map((job) => {
+      {jobs.map((job) => {
         return <KanbanItem job={job} key={job.id} />
       })}
     </div>
--- src/components/kanban/Kanban.jsx
+++ src/components/kanban/Kanban.jsx
@@ -5,11 +5,33 @@ import styles from './kanban.module.css'
 export default function Kanban() {
   const { jobs } = useJobs()
 
+  let wishlistJobs = []
+  let appliedJobs = []
+
+  for (let i = 0; i < jobs.length; i++) {
+    const job = jobs[i]
+    job.wishlist && wishlistJobs.push(job)
+    job.applied && appliedJobs.push(job)
+  }
+
   console.log(jobs)
 
   return (
     <div className={styles.kanbanBoard}>
-      <Column jobs={jobs} columnName={'wishlist'} />
+      <div className={styles.columnTitle}>
+        Wishlist
+        <Column jobs={wishlistJobs} />
+      </div>
+
+      <div className={styles.columnTitle}>
+        Applied
+        <Column jobs={appliedJobs} />
+      </div>
+
+      <div className={styles.columnTitle}>
+        All Jobs
+        <Column jobs={jobs} />
+      </div>
     </div>
   )
 }
